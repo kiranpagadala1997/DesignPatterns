d862b1a06066f7da024bba0245b3dd9f7627c999
kiranpagadala1997 <kiranpagadal17@gmail.com>
2026-05-25 18:50:18 +0530

    prototype design is completed

--- src/main/java/prototypeDesignPattern/Demo.java
+++ src/main/java/prototypeDesignPattern/Demo.java
@@ -2,7 +2,18 @@ package prototypeDesignPattern;
 
 public class Demo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        BookShop b1 = new BookShop();
+        b1.setShopName("Universal");
+        b1.loadData();
+
+
+        BookShop b2 = b1.clone();
+        b1.getBooks().remove(2);
+
+        System.out.println(b1);
+        b2.setShopName("Atlas");
+        System.out.println(b2);
 
     }
 }
